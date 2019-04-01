8f9c58ed2b9f084de2ddd0b05e82edf7eb2c4e18
Daniel Stamer-Squair <drsta3@student.monash.edu>
2019-03-31 19:52:07 -0700

    updated and given to be quite stable

--- peaks.py
+++ peaks.py
@@ -1,14 +1,34 @@
-import numpy as numpy
+import numpy as np
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt#might delete this after testing
 
 def FindPeaksExt(xaxis, yaxis):  #np in please, extended gives indexes too
     indexes = find_peaks(yaxis)#assuming a nice smooth signal
     indexes = indexes[0]
-    xp = xaxis.take[indexes]
-    yp = xaxis.take[indexes]
+    xp = xaxis.take(indexes)
+    yp = yaxis.take(indexes)
     return xp, yp, indexes
 
 def FindPeaks(xaxis, yaxis): #don't return indexes
-    xp, yp, ind = FindPeaksExt(xp, yp)
+    xp, yp, ind = FindPeaksExt(xaxis, yaxis)
     return xp, yp   #reduced return
+
+def PlotPeaks(xaxis, yaxis):
+    xp, yp = FindPeaks(xaxis, yaxis)
+    
+    plt.figure()
+    plt.plot(xaxis, yaxis, color='r', label="data")
+    plt.plot(xp, yp, color='b', linewidth=0, marker=".", markersize=6, label="peak")
+    plt.legend()
+    plt.title("Peaks find example")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
+def TestModule():
+    x = np.linspace(0, 100, 1000)
+    y = np.sin(x) + 0.5 * np.cos(0.5*x)
+    PlotPeaks(x, y)
+
+#run module
+TestModule()
